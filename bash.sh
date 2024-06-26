71e92fff5954e135850cb7de32431a8789398288
shaheryar <shaheryar633@gmail.com>
2024-06-26 18:39:41 +0800

    Bashcode files

--- bash.sh
+++ bash.sh
@@ -1,40 +1,18 @@
 #!/bin/bash
  echo shaheryar
-  
+ 
  #!/bin/bash
 
-# Function to get system name
-get_system_name() {
-    system_name=$(hostnamectl --static)
-    echo "System Name: $system_name"
-}
+# Display system name
+echo "System Name: $(hostname)"
 
-# Function to get CPU information
-get_cpu_info() {
-    cpu_info=$(lscpu | grep "Model name" | awk -F': ' '{print "CPU: " $2}')
-    echo "$cpu_info"
-}
+# Number of running CPUs
+echo "Number of CPUs: $(nproc --all)"
 
-# Function to get total RAM
-get_ram_info() {
-    ram_info=$(free -h | awk '/^Mem:/ {print "RAM: " $2}')
-    echo "$ram_info"
-}
+# RAM information
+echo "RAM (Memory) Information:"
+free -h
 
-# Function to get total SSD storage
-get_total_ssd_storage() {
-    total_ssd_storage=$(lsblk | grep "ssd" | awk '{sum += $4} END {print "Total SSD Storage: " sum}')
-    echo "$total_ssd_storage"
-}
-
-# Main function to display system information
-main() {
-    echo "System Information:"
-    get_system_name
-    get_cpu_info
-    get_ram_info
-    get_total_ssd_storage
-}
-
-# Call the main function
-main
+# HDD information
+echo "HDD (Disk) Usage:"
+df -h"
