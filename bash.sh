0c20b813a8677b6e8543441758f9f11229b48c22
shaheryar <shaheryar633@gmail.com>
2024-06-27 14:48:36 +0800

    Bashcode files

--- bash.sh
+++ bash.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
  echo "Shaheryar Mushtaq"
  
- #!/bin/bash
-
-# Display system name
-echo "Current user (admin): $(whoami)"
 
 # Number of running CPUs
 echo "Number of CPUs: $(nproc --all)"
 
 # RAM information
-echo "RAM (Memory) Information:"
-free -h
+free -h | grep 'Mem:' | awk '{print "Total RAM: " $2}'
+
 
 # HDD information
-echo "HDD (Disk) Usage:"
 df -h | awk '$NF=="/"{printf "Total: %s Used: %s\n", $2, $3}'
